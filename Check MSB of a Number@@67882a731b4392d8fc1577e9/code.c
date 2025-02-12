04c33498d0e7980556acd4b35c08d77260a126f2
Kabirjeet Singh <149439848+Kabirjeet@users.noreply.github.com>
2025-02-12 15:35:50 +0530

    Solution to Check MSB of a Number

--- Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
+++ Check MSB of a Number@@67882a731b4392d8fc1577e9/code.c
@@ -1,1 +1,21 @@
-// Your code here...
+#include <stdio.h>
+#include <limits.h>  // For defining INT_MAX, INT_MIN
+
+int main() {
+    int num;
+    
+    // Input from user
+    scanf("%d", &num);
+
+    // Find the MSB position dynamically
+    int bits = sizeof(int) * 8; // Number of bits in an int
+    int msb_mask = 1 << (bits - 1); // Create a mask with MSB set
+
+    // Check if MSB is set
+    if (num & msb_mask)
+        printf("Set");
+    else
+        printf("Not Set");
+
+    return 0;
+}
