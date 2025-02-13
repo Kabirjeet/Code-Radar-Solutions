77d0ebea7d04cc6c43f18ea481e88912816b31e5
Kabirjeet Singh <149439848+Kabirjeet@users.noreply.github.com>
2025-02-13 11:38:10 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -8,6 +8,7 @@ int main()
     if(isdigit(ch)){
         printf("Digit");
     } else if(isalpha(ch)){
+        ch = tolower(ch);
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
             printf("Vowel");
         } else{
