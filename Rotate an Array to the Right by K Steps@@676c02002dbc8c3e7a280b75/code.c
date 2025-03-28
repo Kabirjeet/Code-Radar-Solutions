d6b5db4501dd2db4eb5dca42e6283a2f44c06897
Kabirjeet Singh <149439848+Kabirjeet@users.noreply.github.com>
2025-03-28 17:01:14 +0530

    Solution to Rotate an Array to the Right by K Steps

--- Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
+++ Rotate an Array to the Right by K Steps@@676c02002dbc8c3e7a280b75/code.c
@@ -10,19 +10,43 @@ int main() {
     }  
 
     scanf("%d",&rot);
-    int rotarr[rot];
-    int rotindex = 0;
+    // int rotarr[rot];
+    // int rotindex = 0;
 
-    for(int i=(size-rot); i<size; i++){
-        rotarr[rotindex] = arr[i];
-        rotindex++;
-    }
+    // for(int i=(size-rot); i<size; i++){
+    //     rotarr[rotindex] = arr[i];
+    //     rotindex++;
+    // }
 
-    for(int i=0; i<rot; i++){
-        printf("%d ",rotarr[i]);
-    }
+    // for(int i=0; i<rot; i++){
+    //     printf("%d ",rotarr[i]);
+    // }
     
-    for(int i=0; i<(size-rot); i++){
+    // for(int i=0; i<(size-rot); i++){
+    //     printf("%d ", arr[i]);
+    // }
+
+    rot = rot % size; // Handle cases where rot > size
+
+    int rotArr[rot];
+
+    // Store the last 'rot' elements
+    for (int i = 0; i < rot; i++) {
+        rotArr[i] = arr[size - rot + i];
+    }
+
+    // Shift remaining elements to the right
+    for (int i = size - 1; i >= rot; i--) {
+        arr[i] = arr[i - rot];
+    }
+
+    // Place rotated elements at the beginning
+    for (int i = 0; i < rot; i++) {
+        arr[i] = rotArr[i];
+    }
+
+    // Print the rotated array
+    for (int i = 0; i < size; i++) {
         printf("%d ", arr[i]);
     }
     return 0;
