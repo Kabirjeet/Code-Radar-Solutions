3af667b9043c8bd0d6a1e20b973d1d1ce9807507
Kabirjeet Singh <149439848+Kabirjeet@users.noreply.github.com>
2025-03-29 16:24:40 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -23,19 +23,12 @@ int main(){
         }
     }
 
-    int min=1, min1=0, min2=0;
-    for(int i=0; i<size; i++){
-        int sub = arr[i];
-
+    int min = arr[size-1] - arr[0], min1=0, min2=0;
+    for(int i=0; i<size-1; i++){
         for(int j=0; j<size; j++){
-            if(i==j){
-                continue;
-            } else{
-                if((arr[j] - arr[i]) <= min){
-                    min = arr[j] - arr[i];
-                    min1 = arr[i];
-                    min2 = arr[j];
-                }
+            if((arr[j] - arr[i]) < min){
+                min1 = arr[i];
+                min2 = arr[j];
             }
         }
     }
